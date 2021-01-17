e5230d60e013dee6eaed095ba8227ba99e057e63
shahnawaz_mobiiworld <shahnawaz.mobiiworld@gmail.com>
2021-01-17 22:16:51 +0530

    problem 1 fixes

--- app/src/main/java/com/android/foodappdemo/problem1/Problem1.kt
+++ app/src/main/java/com/android/foodappdemo/problem1/Problem1.kt
@@ -37,4 +37,12 @@ class Problem1 {
         }
 
     }
+
+
+    /* What is the disadvantage of using the ASCII value of the letters to solve this problem?
+       Ans : 1) ASCII code uses only ENGLISH language.
+             2) A document written in other code can’t be read by ASCII code and it will display some characters only instead of the content.
+             3) maximum 128 characters that is not enough for some key boards having special characters.
+             7bit may not enough to represent larger values. advantage compare to EBCDIC are 7bit so quickly transferable in a fraction of time*/
+
 }
